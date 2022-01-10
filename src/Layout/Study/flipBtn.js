e7a093d70d280b4520c972eac97a80008fca14e7
Bob Ray Vaughn <93263157+BobRayVaughn@users.noreply.github.com>
2022-01-10 02:14:05 -0600

    Delete src/Layout directory

--- src/Layout/Study/flipBtn.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default function FlipButton ({ setIsFrontOfCard }) {
-    const flipCardHandler = () => setIsFrontOfCard((currentSide) => !currentSide)
-    return(
-        <button onClick={flipCardHandler}>Flip</button>
-    )
-}
--- src/Layout/Study/nextBtn.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function NextButton({NextCardHandler}){
-    return(
-        <button onClick={NextCardHandler}>Next</button>
-    )
-}
