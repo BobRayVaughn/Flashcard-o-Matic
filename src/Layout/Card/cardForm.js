da77aaf60d719e2edf7397cf279785c5d8dc12ae
Bob Ray Vaughn <93263157+BobRayVaughn@users.noreply.github.com>
2022-01-10 00:36:51 -0600

    Revision 1

---
+++ src/Layout/Card/cardForm.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function cardForm({cardFront, handleFrontChange, cardBack, handleBackChange}) {
+    return (
+        <div>
+            <div>
+            <label htmlFor="cardFront">Front</label>
+            <textarea
+            id="cardFront"
+            name="cardFront"
+            className="form-control"
+            placeholder="front side of card"
+            rows="3"
+            onChange={handleFrontChange}
+            value={cardFront}
+            />
+            </div>
+            <div>
+                <label htmlFor="cardBack">Back</label>
+                <textarea
+                id="cardBack"
+                name="cardBack"
+                className="form-control"
+                placeholder="Back side of card"
+                rows="3"
+                onChange={handleBackChange}
+                value={cardBack}
+                />
+            </div>
+        </div>
+    )
+}
